78449441e0071f032c1f23dbda6385a7b5f3aad6
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2025-04-11 12:49:28 +0200

    improved MPU setup config and docstring

--- src/main.cpp
+++ src/main.cpp
@@ -68,48 +68,77 @@ void setup()
         }
     }
     Serial.println("MPU6500 is connected!");
-    Serial.println("Position you MPU6500 flat and don't move it - Calibrationg...");
-    delay(1000);
+    Serial.println("Position you MPU6500 flat and don't move it.");
+    delay(2000);
+    Serial.println("Calibrating...");
     myMPU6500.autoOffsets();
     Serial.println("Done");
 
+    /*  You can enable or disable the digital low pass filter (DLPF). If you disable the DLPF, you
+     *  need to select the bandwidth, which can be either 8800 or 3600 Hz. 8800 Hz has a shorter delay,
+     *  but higher noise level. If DLPF is disabled, the output rate is 32 kHz.
+     *  MPU9250_BW_WO_DLPF_3600
+     *  MPU9250_BW_WO_DLPF_8800
+     */
     myMPU6500.enableGyrDLPF();
-    myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
+    //myMPU6500.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800); // bandwidth without DLPF
+
+    /*  Digital Low Pass Filter for the gyroscope must be enabled to choose the level.
+     *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7
+     *
+     *  DLPF    Bandwidth [Hz]   Delay [ms]   Output Rate [kHz]
+     *    0         250            0.97             8
+     *    1         184            2.9              1
+     *    2          92            3.9              1
+     *    3          41            5.9              1
+     *    4          20            9.9              1
+     *    5          10           17.85             1
+     *    6           5           33.48             1
+     *    7        3600            0.17             8
+     *
+     *    You achieve lowest noise using level 6
+     */
+    myMPU6500.setGyrDLPF(MPU6500_DLPF_5);
 
     /*  Sample rate divider divides the output rate of the gyroscope and accelerometer.
-    *  Sample rate = Internal sample rate / (1 + divider)
-    *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
-    *  Divider is a number 0...255
-    */
-    myMPU6500.setSampleRateDivider(5);
+     *  Sample rate = Internal sample rate / (1 + divider)
+     *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
+     *  Divider is a number 0...255
+     */
+    myMPU6500.setSampleRateDivider(99);
 
+    /*  MPU9250_GYRO_RANGE_250       250 degrees per second (default)
+     *  MPU9250_GYRO_RANGE_500       500 degrees per second
+     *  MPU9250_GYRO_RANGE_1000     1000 degrees per second
+     *  MPU9250_GYRO_RANGE_2000     2000 degrees per second
+     */
     myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
 
     /*  MPU9250_ACC_RANGE_2G      2 g
-    *  MPU9250_ACC_RANGE_4G      4 g
-    *  MPU9250_ACC_RANGE_8G      8 g
-    *  MPU9250_ACC_RANGE_16G    16 g
-    */
+     *  MPU9250_ACC_RANGE_4G      4 g
+     *  MPU9250_ACC_RANGE_8G      8 g
+     *  MPU9250_ACC_RANGE_16G    16 g
+     */
     myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
 
     /*  Enable/disable the digital low pass filter for the accelerometer
-    *  If disabled the the bandwidth is 1.13 kHz, delay is 0.75 ms, output rate is 4 kHz
-    */
+     *  If disabled the the bandwidth is 1.13 kHz, delay is 0.75 ms, output rate is 4 kHz
+     */
     myMPU6500.enableAccDLPF(true);
 
     /*  Digital low pass filter (DLPF) for the accelerometer, if enabled
-    *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7
-    *   DLPF     Bandwidth [Hz]      Delay [ms]    Output rate [kHz]
-    *     0           460               1.94           1
-    *     1           184               5.80           1
-    *     2            92               7.80           1
-    *     3            41              11.80           1
-    *     4            20              19.80           1
-    *     5            10              35.70           1
-    *     6             5              66.96           1
-    *     7           460               1.94           1
-    */
-    myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+     *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7
+     *   DLPF     Bandwidth [Hz]      Delay [ms]    Output rate [kHz]
+     *     0           460               1.94           1
+     *     1           184               5.80           1
+     *     2            92               7.80           1
+     *     3            41              11.80           1
+     *     4            20              19.80           1
+     *     5            10              35.70           1
+     *     6             5              66.96           1
+     *     7           460               1.94           1
+     */
+    myMPU6500.setAccDLPF(MPU6500_DLPF_5);
     delay(200);
 
     Serial.print("Connecting to: ");
